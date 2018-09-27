3574e7f7b33cfcedbf85737575b28db3378fe7f7
LENOVO <LENOVO@PB>
2018-09-28 03:14:44 +0700

    I'm god.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Max";
+        return "Hello Mee";
     }
 
     public static void main(String[] args) {
