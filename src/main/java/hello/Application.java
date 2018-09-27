89a769147d7d9cafa5efb3c21a0e7c89e29d655a
LENOVO <LENOVO@PB>
2018-09-28 01:30:09 +0700

    again

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Pakawat";
+        return "Hello Max";
     }
 
     public static void main(String[] args) {
