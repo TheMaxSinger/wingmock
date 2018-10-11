ee8248632503ecc3e784fc7f7788dce6a3c2d135
Pakawat Boonyoung <pakawat@ascendgroup.com>
2018-10-11 12:36:36 +0700

    prometheus

--- src/main/java/pb/Application.java
+++ src/main/java/pb/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+
 @PropertySource(value = {"classpath:/appInfo.properties"}, ignoreResourceNotFound = true)
 @SpringBootApplication
+@EnableSpringBootMetricsCollector
 public class Application {
 
     public static void main(String[] args) {
