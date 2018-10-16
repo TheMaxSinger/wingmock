aff51eab21e060999fa91011989e286711ab7d68
Pakawat Boonyoung <pakawat@ascendgroup.com>
2018-10-16 10:14:52 +0700

    Removing prometheus until I know what's going on

--- src/main/java/pb/Application.java
+++ src/main/java/pb/Application.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+//import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 
 @PropertySource(value = {"classpath:/appInfo.properties"}, ignoreResourceNotFound = true)
 @SpringBootApplication
-@EnableSpringBootMetricsCollector
+//@EnableSpringBootMetricsCollector
 public class Application {
 
     public static void main(String[] args) {
